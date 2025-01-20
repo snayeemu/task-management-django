4035d16859c12b811c2cd26ce993bf698fb4fbcd
snayeemu <snayeemu2@gmail.com>
2025-01-21 01:50:36 +0600

    TaskDetails table added

--- tasks/models.py
+++ tasks/models.py
@@ -8,3 +8,16 @@ class Tasks(models.Model):
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class TaskDetails(models.Model):
+    task = models.OneToOneField(Tasks, on_delete=models.CASCADE)
+    high = "h"
+    medium = "m"
+    low = "l"
+    priority_options = {
+        high: "high",
+        medium: "medium",
+        low: "low",
+    }
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=priority_options, default=low)
